8606c397d12be486d24bb7e054193f9c6413f308
Dan Jacob <danjac2018@gmail.com>
2020-12-10 14:26:03 +0200

    Remove old route

--- chatter/chat/routing.py
+++ chatter/chat/routing.py
@@ -7,5 +7,4 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/$", consumers.ChatConsumer.as_asgi()),
-    re_path(r"ws/chat/(?P<room_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
 ]
